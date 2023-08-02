5c40453a9ae48559502adf76584f915a9ccd5b20
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-08-02 11:42:16 -0700

    inv_mat

--- easy_lausanne.sh
+++ easy_lausanne.sh
@@ -4,6 +4,9 @@ $ easy_lausanne \
      --target_type diffusion \
      --output_dir /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization
 
+
+
+
      $ easy_lausanne \
      --subject_id fsaverage \
      --target_volume /home/zhibinz2/Downloads/ds003505-download/derivatives/cmp-v3.0.3/sub-01/dwi/sub-01_desc-cmp_dwi.nii.gz
