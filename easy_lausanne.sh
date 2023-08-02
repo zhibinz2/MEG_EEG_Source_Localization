c99c20499d1ed6edf6b0f52b37267f108e73f140
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-08-01 21:50:27 -0700

    volume

---
+++ easy_lausanne.sh
@@ -0,0 +1,11 @@
+$ easy_lausanne \
+     --subject_id /home/zhibinz2/mne_data/MNE-sample-data/subjects/fsaverage \
+     --target_volume /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/CAMCAN_inverse.nii.gz \
+     --target_type diffusion \
+     --output_dir /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization
+
+     $ easy_lausanne \
+     --subject_id fsaverage \
+     --target_volume /home/zhibinz2/Downloads/ds003505-download/derivatives/cmp-v3.0.3/sub-01/dwi/sub-01_desc-cmp_dwi.nii.gz
+     --target_type diffusion \
+     --output_dir /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization
