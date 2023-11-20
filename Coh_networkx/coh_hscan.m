cad667428008e44dd9f04f24414480716a6fa52e
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-11-20 13:46:59 -0800

    zscore

--- Coh_networkx/coh_hscan.m
+++ Coh_networkx/coh_hscan.m
@@ -24,9 +24,180 @@ for freq=1:5
 end
 % plot
 figure
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
 for freq=1:5
     subplot(1,5,freq)
     imagesc(squeeze(coh_mean_5freq(freq,:,:)));colorbar
+    title(bandlabels{freq})
 end
 
+coh_all_5freq=coh_5freq;
 %%
+cd /home/zhibinz2/Documents/GitHub/Cleaned_data/hilbert_datacov
+load('Pcoh_boolean.mat')
+
+% put same frequency together in one of 5 cells
+coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            tic
+            for freq=1:5
+                coh_5freq{freq}=cat(3,coh_5freq{freq},squeeze(Pcoh_boolean(ses,subj,tr,freq,:,:)));
+            end
+            toc
+        end
+    end
+end
+% mean
+coh_mean_5freq=nan(5,448,448);
+for freq=1:5
+    coh_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+end
+% plot
+figure
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+clf
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh_mean_5freq(freq,:,:)));colorbar
+    colormap('hotncold')
+    title(bandlabels{freq})
+end
+sgtitle('boolean partial coherence avearged in source space')
+
+Pcoh_boolean_5freq=coh_5freq;
+
+coh_5freq=Pcoh_boolean_5freq;
+
+% convert to zscore
+coh_5all=cat(3,coh_5freq{1},coh_5freq{2},coh_5freq{3},coh_5freq{4},coh_5freq{5});
+coh_5zscore=zscore(coh_5all,[],3);
+% mean
+coh32_mean_5freq=nan(5,448,448);
+for freq=1:5
+    coh32_mean_5freq(freq,:,:)=mean(coh_5zscore(:,:,[[1:288]+288*(freq-1)]),3);
+end
+% plot
+figure
+clf
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh32_mean_5freq(freq,:,:)));colorbar
+    colormap('jet')
+    title(bandlabels{freq})
+    clim([-0.5 0.5])
+end
+sgtitle('zscore: boolean partial coherence in source space averaged')
+
+%%
+cd /home/zhibinz2/Documents/GitHub/Cleaned_data/hilbert_datacov
+clear
+load('Pcoh_all.mat')
+
+% put same frequency together in one of 5 cells
+coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            tic
+            for freq=1:5
+                coh_5freq{freq}=cat(3,coh_5freq{freq},squeeze(Pcoh_all(ses,subj,tr,freq,:,:)));
+            end
+            toc
+        end
+    end
+end
+% mean
+coh_mean_5freq=nan(5,448,448);
+for freq=1:5
+    coh_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+end
+% plot
+figure
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+clf
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh_mean_5freq(freq,:,:)));colorbar
+    colormap('jet')
+    title(bandlabels{freq})
+    clim([0 0.001])
+end
+sgtitle('partial coherence avearged in source space')
+
+Pcoh_all_5freq=coh_5freq;
+%%
+% cd /home/zhibinz2/Documents/GitHub/Cleaned_data/hilbert_datacov
+% clear
+% load('coh_all.mat')
+% 
+% % put same frequency together in one of 5 cells
+% coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+% for ses=1:12
+%     for subj=1:2
+%         for tr=1:12
+%             tic
+%             for freq=1:5
+%                 coh_5freq{freq}=cat(3,coh_5freq{freq},squeeze(coh_all(ses,subj,tr,freq,:,:)));
+%             end
+%             toc
+%         end
+%     end
+% end
+% % mean
+% coh_mean_5freq=nan(5,448,448);
+% for freq=1:5
+%     coh_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+% end
+% % plot
+% figure
+% bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+% clf
+% for freq=1:5
+%     subplot(1,5,freq)
+%     imagesc(squeeze(coh_mean_5freq(freq,:,:)));colorbar
+%     colormap('jet')
+%     title(bandlabels{freq})
+% end
+% sgtitle('ordinary coherence avearged in source space')
+
+%%
+load('/ssd/zhibin/Cleaned_sourcedata/cortical_source_data/python_lasso/Pcoh_lasso.mat')
+% put same frequency together in one of 5 cells
+coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            tic
+            for freq=1:5
+                coh_5freq{freq}=cat(3,coh_5freq{freq},squeeze(Pcoh_lasso(ses,subj,tr,freq,:,:)));
+            end
+            toc
+        end
+    end
+end
+% mean
+coh_mean_5freq=nan(5,448,448);
+for freq=1:5
+    coh_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+end
+% plot
+figure
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+clf
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh_mean_5freq(freq,:,:)));colorbar
+    colormap('jet')
+    title(bandlabels{freq})
+    clim([0 0.001])
+end
+sgtitle('partial coherence by lasso averaged in source space')
+
+Pcoh_lasso_5freq=coh_5freq;
+
+%%
+cd /home/zhibinz2/Documents/GitHub/Cleaned_data/hilbert_datacov
+save('source_5freq.mat','Pcoh_lasso_5freq','coh_all_5freq','Pcoh_boolean_5freq','Pcoh_all_5freq','-v7.3');
--- chan32_pcoh/loop_data.m
+++ chan32_pcoh/loop_data.m
@@ -35,3 +35,224 @@ for ses=1:12
 end
 
 %% convert back to complex and then normalize to cross spetra
+% convert to complex covariance
+cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
+Complex_prec_all=nan(12,2,12,5,32,32);
+for ses=1:numSes
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                Complex_prec_all(ses,subj,tr,freq,:,:)=r2c(squeeze(chan_prec_all(ses,subj,tr,freq,:,:)));
+            end
+        end
+    end
+end
+
+% normalize to real coherence
+cd /home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh/Simulations/util
+coh32_all=nan(12,2,12,5,32,32);
+for ses=1:numSes
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                coh32_all(ses,subj,tr,freq,:,:)=normalizeCSD(squeeze(Complex_prec_all(ses,subj,tr,freq,:,:)));
+            end
+        end
+    end
+end
+
+% examine all trial
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                imagesc(squeeze(coh32_all(ses,subj,tr,freq,:,:)));
+                colorbar;
+                colormap('jet')
+                % clim([-1 1]);
+                title(['ses ' num2str(ses) ...
+                    ' subj ' num2str(subj) ...
+                    ' tr ' num2str(tr) ...
+                    ' freq ' num2str(freq)])
+                pause(0.1);
+            end 
+        end
+    end
+end
+
+
+% put same frequency together in one of 5 cells
+coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            tic
+            for freq=1:5
+                coh_5freq{freq}=cat(3,coh_5freq{freq},logical(squeeze(coh32_all(ses,subj,tr,freq,:,:))));
+            end
+            toc
+        end
+    end
+end
+% mean
+coh32_mean_5freq=nan(5,32,32);
+for freq=1:5
+    coh32_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+end
+% plot
+figure
+clf
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh32_mean_5freq(freq,:,:)));colorbar
+     colormap('jet')
+    title(bandlabels{freq})
+end
+sgtitle('boolean partial coherence in channel space averaged')
+
+chan_pcoh_boolean_5freq=coh_5freq;
+
+coh_5freq=chan_pcoh_boolean_5freq;
+
+
+% convert to zscore
+coh_5all=cat(3,coh_5freq{1},coh_5freq{2},coh_5freq{3},coh_5freq{4},coh_5freq{5});
+coh_5zscore=zscore(coh_5all,[],3);
+% mean
+coh32_mean_5freq=nan(5,32,32);
+for freq=1:5
+    coh32_mean_5freq(freq,:,:)=mean(coh_5zscore(:,:,[[1:288]+288*(freq-1)]),3);
+end
+% plot
+figure
+clf
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh32_mean_5freq(freq,:,:)));colorbar
+    colormap('jet')
+    title(bandlabels{freq})
+    clim([-0.5 0.5])
+end
+sgtitle('zscore: boolean partial coherence in channel space averaged')
+
+
+
+%% examine all trial
+load('chan_cov_all.mat')
+ses=12;
+subj=2;
+tr=12;
+freq=5;
+imagesc(squeeze(chan_cov_all(ses,subj,tr,freq,:,:)));
+colorbar;
+
+figure
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                imagesc(squeeze(chan_cov_all(ses,subj,tr,freq,:,:)));
+                colorbar;
+                colormap('jet')
+                % clim([-1 1]);
+                title(['ses ' num2str(ses) ...
+                    ' subj ' num2str(subj) ...
+                    ' tr ' num2str(tr) ...
+                    ' freq ' num2str(freq)])
+                pause(0.1);
+            end 
+        end
+    end
+end
+
+%% convert back to complex and then normalize to cross spetra
+% convert to complex covariance
+cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
+Complex_cov_all=nan(12,2,12,5,32,32);
+for ses=1:numSes
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                Complex_cov_all(ses,subj,tr,freq,:,:)=r2c(squeeze(chan_cov_all(ses,subj,tr,freq,:,:)));
+            end
+        end
+    end
+end
+
+% normalize to real coherence
+cd /home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh/Simulations/util
+coh32_all=nan(12,2,12,5,32,32);
+for ses=1:numSes
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                coh32_all(ses,subj,tr,freq,:,:)=normalizeCSD(squeeze(Complex_cov_all(ses,subj,tr,freq,:,:)));
+            end
+        end
+    end
+end
+
+% examine all trial
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            for freq=1:5
+                imagesc(squeeze(coh32_all(ses,subj,tr,freq,:,:)));
+                colorbar;
+                colormap('jet')
+                % clim([-1 1]);
+                title(['ses ' num2str(ses) ...
+                    ' subj ' num2str(subj) ...
+                    ' tr ' num2str(tr) ...
+                    ' freq ' num2str(freq)])
+                pause(0.1);
+            end 
+        end
+    end
+end
+
+
+% put same frequency together in one of 5 cells
+coh_5freq=cell(1,5); % 448 x 448 x 288 trials
+for ses=1:12
+    for subj=1:2
+        for tr=1:12
+            tic
+            for freq=1:5
+                coh_5freq{freq}=cat(3,coh_5freq{freq},squeeze(coh32_all(ses,subj,tr,freq,:,:)));
+            end
+            toc
+        end
+    end
+end
+% mean
+coh32_mean_5freq=nan(5,32,32);
+for freq=1:5
+    coh32_mean_5freq(freq,:,:)=mean(coh_5freq{freq},3);
+end
+chan_coh_5freq=coh_5freq;
+cd /home/zhibinz2/Documents/GitHub/Cleaned_data/hilbert_datacov
+save('chan_5freq.mat','chan_pcoh_boolean_5freq','chan_coh_5freq','-v7.3')
+
+% convert to zscore
+coh_5all=cat(3,coh_5freq{1},coh_5freq{2},coh_5freq{3},coh_5freq{4},coh_5freq{5});
+coh_5zscore=zscore(coh_5all,[],3)
+% mean
+coh32_mean_5freq=nan(5,32,32);
+for freq=1:5
+    coh32_mean_5freq(freq,:,:)=mean(coh_5zscore(:,:,[[1:288]+288*(freq-1)]),3);
+end
+
+% plot
+figure
+clf
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+for freq=1:5
+    subplot(1,5,freq)
+    imagesc(squeeze(coh32_mean_5freq(freq,:,:)));colorbar
+     colormap('jet')
+    title(bandlabels{freq})
+end
+sgtitle('ordinary coherence in channel space averaged')
