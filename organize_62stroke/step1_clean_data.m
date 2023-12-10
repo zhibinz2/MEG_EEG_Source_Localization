3306f020ba538fd7ca871095a9938b8116a07291
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-12-10 10:07:27 -0800

    agl

--- organize_62stroke/step1_clean_data.m
+++ organize_62stroke/step1_clean_data.m
@@ -202,3 +202,206 @@ for f=1:61
     display(['Complete one file: ' num2str(subj_files(f)) '.mat ********************'])
     toc
 end
+
+%% % source localization and pca to get cortical source data
+% load forward matrix and source
+clear
+cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/HEAD_model_ESCH
+load('ESCH_ico_4_scale_0.05_depth_0.8.mat','leadfield','source_rr');
+load('Lausanne2008_fsaverageDSsurf_60_125_250.mat','Brain', ...
+    'roiNames_250','scale250_subcortROIs')
+load('parcels.mat') % This is the labels
+% label the sources
+Vertex=Brain.Vertex;
+% Anni's labeling method
+x_shift=(max(Vertex(:,1))-max(source_rr(:,1))*1e3)/2+(min(Vertex(:,1))-min(source_rr(:,1))*1e3)/2;
+y_shift=(max(Vertex(:,2))-max(source_rr(:,2))*1e3)/2+(min(Vertex(:,2))-min(source_rr(:,2))*1e3)/2;
+z_shift=(max(Vertex(:,3))-max(source_rr(:,3))*1e3)/2+(min(Vertex(:,3))-min(source_rr(:,3))*1e3)/2;
+source_x=source_rr(:,1) * 1e3 + x_shift;
+source_y=source_rr(:,2) * 1e3 + y_shift;
+source_z=source_rr(:,3) * 1e3 + z_shift;
+source_xyz=[source_x source_y source_z];
+num_source=size(source_xyz,1);
+source_fsaverage = source_xyz+127.5; % 127.5 is based on the fsaverage volume being 256 x 256 x 256
+source_labels=zeros(num_source,1);
+for i = 1:length(source_fsaverage)
+    vox = floor(source_fsaverage(i,:)); % change from ceil to floor,now we have 2 subcortical not mapped
+    inds              = sub2ind([size(parcels)], vox(1), vox(2), vox(3));
+    label             = parcels(inds); 
+    source_labels(i) = label;
+end
+% find which label not mapped
+roiNames_250(setdiff(1:463,unique(source_labels))) % 227 457 459
+setdiff(1:463,unique(source_labels))
+
+% get coordinate and label for each aggregated ROIs
+ave_source_coor=[];
+ave_source_label=[];
+for sr=1:max(unique(source_labels))
+    I=find(source_labels==sr);
+    if ~isempty(I)
+        ave_source_coor=[ave_source_coor; mean(source_fsaverage(I,:),1)];
+        ave_source_label=[ave_source_label; sr];
+    end
+end 
+
+% create a boolean of subcortical rois
+bool_subcorti=zeros(1,length(ave_source_label));
+for i=1:length(ave_source_label)
+    clear tmp
+    tmp=ave_source_label(i);
+    if ismember(tmp, scale250_subcortROIs)
+        bool_subcorti(i)=1;
+    end
+end
+sum(bool_subcorti)
+ind=find(bool_subcorti);
+ind % use these subcortical indices to remove subcortical aggregated pca data
+% remove subcortical roi
+ave_source_coor(ind,:)=[];
+ave_source_label(ind)=[];
+corti_ave_source_coor=ave_source_coor;
+corti_ave_source_labl=ave_source_label;
+cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_corti_source
+save('corti_ave_source_coor.mat','corti_ave_source_coor');
+save('corti_ave_source_labl.mat','corti_ave_source_labl');
+
+% get inverse model
+addpath ../../AdaptiveGraphicalLassoforParCoh/Simulations/util
+[inversemat, stat, reconstructed] = inversemodel(leadfield,'prctile',1);
+
+
+
+% source loclization and pca
+subj_files=[0:1:60];
+for f=1:61
+    tic
+    cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_cleaned/
+    load([num2str(subj_files(f)) '.mat'],'preprocessed_eeg','subject_ID', ...
+        'chanlocs','ch_labels','ch_dubious','ch_peripheral','Fs');
+    display(['start processing subject file: ' num2str(subj_files(f)) '.mat']);
+
+    source_data=inversemat*preprocessed_eeg;
+
+    fra_eigenvalues=zeros(1,max(unique(source_labels)));
+    corti_source_data=[];
+    for sr=1:max(unique(source_labels))
+        I=find(source_labels==sr);
+        if ~isempty(I)
+            [COEFF, SCORE, LATENT] = pca(source_data(I,:)','Centered',false);
+            fra_eigenvalues(sr)=LATENT(1)/sum(LATENT);
+            corti_source_data=[corti_source_data SCORE(:,1)];
+        end
+    end % 66s
+
+    % remove the non-zeros fraction of eigenvalues and subcortical rois
+    corti_fra_eigenvalues=fra_eigenvalues(ave_source_label);
+    
+    % remove subcortical roi
+    corti_source_data(:,ind)=[];
+
+    cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_corti_source
+    save([num2str(subject_ID) '.mat'],'corti_fra_eigenvalues', ...
+        'corti_source_data','corti_ave_source_coor','corti_ave_source_labl', ...
+        'subject_ID','chanlocs','ch_labels','ch_dubious','ch_peripheral','Fs');
+
+    display(['Complete one file: ' num2str(subj_files(f)) '.mat ********************'])
+    toc
+
+end
+
+%% downsample filter hilbert -> coh and partial coh
+% refer to 
+% open /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL/hilbert2cov.m
+clear
+Fs=1000;
+srnew = 200;
+downsample = Fs/srnew;
+passbands = [3.5 6.5; 7 10; 10.5 13.5; 14 20; 21 29];
+bandlabels = {'Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+attenuation=60;
+filt_ds=cell(1,length(passbands));
+for freqBand=1:5
+    % Select frequency
+    passFreq1 = passbands(freqBand,1);
+    passFreq2 = passbands(freqBand,2);
+    filt_d = designfilt('bandpassiir','FilterOrder',20, ...
+    'PassbandFrequency1',passFreq1,'PassbandFrequency2',passFreq2, ...
+    'StopbandAttenuation1',attenuation,'PassbandRipple',0.2, ...
+    'StopbandAttenuation2',attenuation,'SampleRate',srnew);
+    filt_ds{freqBand}=filt_d;
+end
+
+% AGL variables
+allLambdas = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+allLambdasOut = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+n_Lambdas=length(allLambdas); % number of lambda values
+min_LamdaIn = min(allLambdas);
+cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_corti_source
+load('corti_ave_source_labl.mat'); % every trial is the same, load one trial
+% penaltyselection for 1 subject
+addpath(genpath('/../../AdaptiveGraphicalLassoforParCoh/Simulations/util'))
+addpath(genpath('../../AdaptiveGraphicalLassoforParCoh/AGL'))
+load('../../Virtual-Tractography/ForZhibin/processed_data/scale250_Connectome.mat','fc');
+SC=logical(fc(corti_ave_source_labl,corti_ave_source_labl));
+sum(triu(SC,1),"all") % 4990 edges
+
+% penalty selection and fit precision
+subj_files=[0:1:60];
+for f=1:2
+    tic
+    cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_corti_source
+    load([num2str(subj_files(f)) '.mat'],'corti_source_data','subject_ID', ...
+        'chanlocs','ch_labels','ch_dubious','ch_peripheral','Fs');
+    display(['start processing subject file: ' num2str(subj_files(f)) '.mat']);
+
+    downsample_data=resample(double(corti_source_data),1,downsample,'Dimension',1);
+    
+    datapermuted=cell(1,5);
+    stroke_Cov=cell(1,5); % coh
+    stroke_Pcov=cell(1,5); % partial coh
+    penalizationIn_op=nan(1,5);
+    penalizationOut_op=nan(1,5);
+    minDev_op=nan(1,5);
+    for freq=1:5
+        filtered_data = filter(filt_ds{freq},downsample_data);
+        hilbertdata = hilbert(filtered_data');
+        sourceDataReal = cat(1,real(hilbertdata),imag(hilbertdata));
+        sourceDataReal = [sourceDataReal*(1/mean(abs(sourceDataReal(:))))]'; % normalize data
+        % split into 4 ensambles: 4 x #source x #(samples/4)
+        n_split=2; n_sr=size(sourceDataReal,2);
+        sam_len=size(sourceDataReal,1);
+        sam_size=floor(sam_len/n_split); sam_range=1:sam_size;
+        sourceDataReal = sourceDataReal(1:n_split*sam_size,:);
+        datareshaped = reshape(sourceDataReal, sam_size, n_split, n_sr);
+        datapermuted{freq} = permute(datareshaped,[2,3,1]); % split into 2 ensambles: 2x896x9300
+        % compute covariance
+        stroke_Cov{freq} = cov(sourceDataReal);
+        for n=1:n_split
+            datapermuted_cov{freq}(n,:,:) = cov([squeeze(datapermuted{freq}(n,:,:))]');
+        end
+
+        clear corti_source_data filtered_data  hilbertdata
+        clear sourceDataReal datareshaped 
+
+        % AGL
+        cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
+        dataCovs_op=squeeze(datapermuted_cov{freq});
+        [penalizationIn_op(freq),penalizationOut_op(freq),minDev_op(freq)]=penaltyselection( ...
+        SC,allLambdas,allLambdasOut,dataCovs_op);
+        [stroke_Pcov{freq}] = fitprecision( ...
+        SC,penalizationIn_op(freq),penalizationOut_op(freq),min_LamdaIn,stroke_Cov{freq});
+        
+        clear datapermuted_cov dataCovs_op 
+
+    end
+    clear downsample_data datapermuted
+
+    cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_source_hilbert
+    save([num2str(subject_ID) '.mat'],'stroke_Cov', 'stroke_Pcov', ...
+        'penalizationIn_op','penalizationOut_op','minDev_op', ...
+        'subject_ID','chanlocs','ch_labels','ch_dubious','ch_peripheral','Fs');
+
+    display(['Complete one file: ' num2str(subj_files(f)) '.mat ********************'])
+    toc
+end
